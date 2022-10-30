9a491a1fe4c73178b09a9b60a9004a5f7db8356b
galang8664 <95033122+galang8664@users.noreply.github.com>
2022-10-30 20:00:38 +0700

    lava-RiceDroid-thirteen-galang8664

--- build_rom.sh
+++ build_rom.sh
@@ -1,12 +1,23 @@
 # sync rom
+
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/MiDoNaSR545/mainfest_personal -b los --depth 1 -b master .repo/local_manifests
+
+git clone https://github.com/galang8664/local_manifest.git --depth 1 -b rice-r .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom [1]
+
 source build/envsetup.sh
-export TZ=Asia/Dhaka #put before last build command
-brunch dream2lte
+
+lunch lineage_lava-userdebug
+
+export SELINUX_IGNORE_NEVERALLOWS=true
+
+export TZ=Asia/Dhaka
+
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
+
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
